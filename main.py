1f7fcc61d0cc07d3140a4e817b92734e2a70715c
THARUN <95579524+Tharunmathi@users.noreply.github.com>
2022-02-19 21:02:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+for i in range(5):
+    for j in range(i+1):
+        print("*",end =" ")
+    print()
+print("###############################################")
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end = " ")
+    print()
+print("###############################################")
+
+for i in range(1,6):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+print("###############################################")
+
+for i in range(5,0,-1):
+    for j in range(i):
+        print(i,end= " ")
+    print()
+print("###############################################")
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end = " ")
+    print()
+print("###############################################")
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end = " ")
+    print()
+print("###############################################")
+
+for i in range(1,6):
+    for j in range(i,0,-1):
+        print(j,end = " ")
+    print()
+print("###############################################")
+
+for i  in range(6,0,-1):
+    for j in range(i,0,-1):
+        print(j,end = " ")
+    print()
+print("###############################################")
+
+col = 1
+n = 1
+for i in range(1,6):
+    for j in range(1,col+1):
+        print(n,end =" ")
+        n = n+1
+    col =col+2
+    print()
+print("###############################################")
+for i in range(1,6):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(1,i+1):
+        print("*",end = " ")
+    print()
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+for i in range(5,0,-1):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(1,i+1):
+        print("*",end = " ")
+    print()
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+for i in range(1,6):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(1,i+1):
+        print(k,end = " ")
+    print()
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+for i in range(5,0,-1):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(1,i+1):
+        print(k,end = " ")
+    print( )
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+for i in range(1,6):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(1,i+1):
+        print(i,end = ' ')
+    print()
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+for i in range(5,0,-1):
+    for j in range(5,i,-1):
+        print(" ",end = ' ')
+    for k in range(1,i+1):
+        print(i,end  = " ")
+    print()
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
